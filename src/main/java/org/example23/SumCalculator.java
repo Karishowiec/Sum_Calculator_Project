e627ee367adbce8ac81c557547f2ebe4190ed13a
Oleksandr Kharchuk <karishowiec32@gmail.com>
2024-01-25 13:48:50 +0100

    add

--- src/main/java/org/example23/SumCalculator.java
+++ src/main/java/org/example23/SumCalculator.java
@@ -1,2 +1,16 @@
-package org.example23;public class SumCalculator {
+package org.example23;
+
+public class SumCalculator {
+
+    public int sum(int n) {
+        if (n <= 0 ) {
+            throw new IllegalArgumentException("N <= 0");
+        }
+        int sum = 0;
+
+        for (int i = 1; i <= n; i++) {
+            sum += i;
+        }
+        return sum;
+    }
 }
--- src/test/java/org/example23/SumCalculatorTest.java
+++ src/test/java/org/example23/SumCalculatorTest.java
@@ -1,4 +1,39 @@
+package org.example23;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class SumCalculatorTest {
-  
+    SumCalculator sumCalculator;
+    @BeforeEach
+    void setUp() {
+        sumCalculator = new SumCalculator();
+
+
+    }
+
+    @Test
+    void sumForThree() {
+        int sum = sumCalculator.sum(3);
+        Assertions.assertEquals(6, sum);
+
+    }
+
+    @Test
+    void sumForZero() {
+        Assertions.assertThrows(
+                IllegalArgumentException.class,
+                ()->sumCalculator.sum(0)
+        );
+    }
+
+    @Test
+    void sumForOne() {
+        int sum = sumCalculator.sum(1);
+        Assertions.assertEquals(1, sum);
+
+    }
 }
